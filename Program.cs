a3febd3a42703228c6349d2e5c39dfd0c7598f54
Naruto <dolphi228@gmail.com>
2025-09-15 16:53:50 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Linq;
+//int[] numbers = [1, 2, 3, 4, 5];
+//int[] numbers2 = new int[5] { 1, 2, 3, 4, 5 };
+//numbers[0] = 32;
+//Console.WriteLine(numbers[1]);
+//Console.WriteLine(numbers.Length);
+
+//foreach(var i in numbers) {
+//    Console.WriteLine(i);
+//}
+
+//for (int i = 0; i < numbers.Length; i++) {
+//    Console.WriteLine(numbers[i]);
+//}
+
+//dynamic[] t = [1, 2, 3, 4, "ss"];
+//object[] t2 = [1, 2, 3, 4, "ss"];
+
+//List<string> people = ["daa", "xcv", "seeer"];
+//List<string> people1 = new List<string> ["daa", "xcv", "seeer"];
+//Console.WriteLine(people.Count);
+
+//foreach (var i in people) {
+//    Console.WriteLine(i);
+//}
+//people.Add("jjj");
+//people.Remove("daa");
+//people.RemoveAt(1);
+//people.Insert(0, "aa");
+
+// Задание 1
+string[] obj = ["sword", "shield", "potion", "sword", "potion", "potion"];
+
+// Задание 2
+int[] num = [2, 4, 5, 6, 2, 1];
+int n_sum = num.Sum();
+Console.WriteLine(n_sum);
+
+// Задание 3
+int[] num1 = [2, 3, 4, 5, 6];
+int maxx = num1[0];
+
+for (int i = 1; i < num1.Length; i++) {
+    if (num1[i] > maxx) {
+        maxx = num1[i];
+    }
+}
+Console.WriteLine(maxx);
+
+// Задание 4
+int[] num2 = [1, 2, 3, 4, 5,];
+int chetnoe = 0;
+int nechentoe = 0;
+
+foreach (int i in num2) {
+    if (i % 2 == 0) {
+        chetnoe++;
+    }
+    else {
+        nechentoe++;
+    }
+}
+Console.WriteLine($"чётные: {chetnoe}\nне чётные: {nechentoe}");
+
+// Здание 5
+int[] num3 = [120, 95, 110, 105, 100];
+int faster = num3.Min();
+Console.WriteLine(faster);
+
+// Задание 6
+string[] t = ["win", "loss", "win", "win", "loss"];
+int kw = 0;
+int kl = 0;
+
+foreach (string h in t) {
+    if (h == "win") {
+        kw++;
+    }
+    else {
+        kl++;
+    }
+}
+Console.WriteLine($"побед: {kw}\nпоражений: {kl}");
+
+// Задание 7
+int[] num4 = [15, 20, 25, 30, 10];
+double aver = num4.Average();
+Console.WriteLine(aver);
+
+// 8 Задание 8
+string[] t2 = ["Меч", "Ласточка", "Бомба", "Зелье", "Гром", "Ласточка"];
+foreach (string h2 in t2) {
+    if (h2 == "Ласточка") {
+        t2[h2].Replace("Ласточка", "Кошка");
+    }
+}
